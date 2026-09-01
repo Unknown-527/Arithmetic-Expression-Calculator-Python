ccbc6449e65782694030dfd57beff2f6d133db6b
Unknown_527 <iice.mralam2010@gmail.com>
2026-09-01 16:23:13 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -7,26 +7,31 @@ def main():
         except:
             expressionl.append(i)
     ch=input("Enter A for BODMAS or B for PEMDAS: ")
-    if ch.lower()=="a":
-        bodmas(expressionl)
-    elif ch.lower()=="b":
-        pemdas(expressionl)
-    else:
-        print("Wrong Choice")
+    try:
+        if ch.lower()=="a":
+            bodmas(expressionl)
+        elif ch.lower()=="b":
+            pemdas(expressionl)
+        else:
+            print("Wrong Choice")
+    except:
+            print("Error, you did something ")
         
 def bodmas(n):
+    n=of(n)
     n=div(n)
     n=pro(n)
     n=addit(n)
     n=subit(n)
-    print("Result: ",n)
+    print("Result: ",float(n))
 
 def pemdas(n):
+    n=exp(n)
     n=pro(n)
     n=div(n)
     n=addit(n)
     n=subit(n)
-    print("Result: ",n)
+    print("Result: ",float(n))
             
 def div(n):
     print("div")
@@ -40,7 +45,7 @@ def div(n):
     return n
 def pro(n):
     print("pro")
-    while "/" in n:
+    while "*" in n:
             a=n[n.index("*")-1]*n[n.index("*")+1]
             n.insert(n.index("*")-1,a)
             n.pop(n.index("*")-1)
@@ -70,5 +75,27 @@ def subit(n):
             n.pop(n.index("-"))
             print (n)
     return n
-    
+
+def of(n):
+    print("of")
+    while "of" in n:
+        a=n[n.index("of")-1]*n[n.index("of")+1]
+        n.insert(n.index("of")-1,a)
+        n.pop(n.index("of")-1)
+        n.pop(n.index("of")+1)
+        n.pop(n.index("of"))
+        print (n)
+    return n
+
+def exp(n):
+    print("exponent")
+    while "^" in n:
+        a=n[n.index("^")-1]**n[n.index("^")+1]
+        n.insert(n.index("^")-1,a)
+        n.pop(n.index("^")-1)
+        n.pop(n.index("^")+1)
+        n.pop(n.index("^"))
+        print (n)
+    return n
+
 main()
